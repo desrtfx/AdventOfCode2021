2e6aaaadfe36b62747de748b9f6f5a5a4fae2866
desrtfx <desrtfx@gmail.com>
2021-12-10 10:24:33 +0100

    Day09 b

--- src/day09/Day09.java
+++ src/day09/Day09.java
@@ -11,6 +11,9 @@ public class Day09 {
 
 	static int[][] heightmap;
 	static List<Point> neighbors;
+	static int height;
+	static int width;
+	static List<Point> localMins = new ArrayList<>();
 
 	static {
 
@@ -22,11 +25,21 @@ public class Day09 {
 
 		try {
 			List<String> lines = Files.readAllLines(Paths.get("Input/Day09.txt"));
-			heightmap = new int[lines.size()][lines.get(0).length()];
+			height = lines.size() + 2;
+			width = lines.get(0).length() + 2;
+			heightmap = new int[height][width];
 			for (int row = 0; row < lines.size(); row++) {
 				for (int col = 0; col < lines.get(row).length(); col++) {
-					heightmap[row][col] = (int) (lines.get(row).charAt(col) - '0');
+					heightmap[row + 1][col + 1] = (int) (lines.get(row).charAt(col) - '0');
 				}
+			}
+			for (int col = 0; col < width; col++) {
+				heightmap[0][col] = 9;
+				heightmap[height - 1][col] = 9;
+			}
+			for (int row = 0; row < height; row++) {
+				heightmap[row][0] = 9;
+				heightmap[row][width - 1] = 9;
 			}
 
 		} catch (IOException e) {
@@ -37,20 +50,18 @@ public class Day09 {
 
 	public static int part01(int[][] heightmap) {
 		int sum = 0;
-		for (int row = 0; row < heightmap.length; row++) {
-			for (int col = 0; col < heightmap[row].length; col++) {
+		for (int row = 1; row < height - 1; row++) {
+			for (int col = 1; col < width - 1; col++) {
 				int h = heightmap[row][col];
 				boolean localMin = true;
 				for (Point p : neighbors) {
-					if ((row + p.y >= 0) && (row + p.y < heightmap.length) && (col + p.x >= 0)
-							&& (col + p.x < heightmap[row].length)) {
-						if (h >= heightmap[row + p.y][col + p.x]) {
-							localMin = false;
-						}
+					if (h >= heightmap[row + p.y][col + p.x]) {
+						localMin = false;
 					}
 				}
 				if (localMin) {
 					sum += (h + 1);
+					localMins.add(new Point(col, row));
 				}
 			}
 		}
