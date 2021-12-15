4b06a4c02a51d3001a7219b179ab3cf2dfd49d8f
desrtfx <desrtfx@gmail.com>
2021-12-15 06:01:04 +0100

    Day15 prep

--- src/day15/Day15.java
+++ src/day15/Day15.java
@@ -1,5 +1,22 @@
 package day15;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Day15 {
 
+	
+	static {
+		try {
+			List<String> lines = Files.readAllLines(Paths.get("Input/Day09.txt"));
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+	
+	
 }
